6d7ae60921b6e3ed7d89c733d32bcbd004d596c4
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-05-01 23:48:02 -0700

    Get info for a movie through OMDB API

--- movieThis.js
+++ movieThis.js
@@ -5,7 +5,37 @@ const axios = require("axios");
  * Looks up info for a movie
  */
 function movieThis() {
-    console.log("In movieThis.js");
+    // Initialize movie to default "Mr. Nobody"
+    let movie = "Mr.%20Nobody,";
+    
+    // If the user inputted a movie name
+    if (process.argv.length > 3) {
+        // Change movie to the input movie name
+        movie = process.argv.slice(3).join("%20");
+    }
+
+    // Get info for the movie
+    getMovieInfo(movie);
+}
+
+/**
+ * Get movie info from the the given movie name string
+ * @param {string} movie 
+ */
+function getMovieInfo(movie) {
+    // OMDB API endpoint URL
+    let queryUrl = `http://www.omdbapi.com/?apikey=trilogy&t=${movie}`
+
+    // Request using URL
+    axios.get(queryUrl)
+        // On success
+        .then(function (response) {
+            console.log(JSON.stringify(response.data, null, 2));
+        })
+        // If there's an error
+        .catch(function (error) {
+            console.log(error);
+        })
 }
 
 // Export the movieThis function so that it can be included in other files
