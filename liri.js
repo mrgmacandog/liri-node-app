65d7c9219116f33b0c895ecde6dfcbd7d2cd1354
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-05-01 18:35:40 -0700

    Add switch statement to trigger commands

--- liri.js
+++ liri.js
@@ -2,7 +2,54 @@
 require("dotenv").config();
 
 // Save keys into a variable
-var keys = require("./keys.js");
+const keys = require("./keys.js");
 
-// Save Spotify object into a variable
-var spotify = new Spotify(keys.spotify);
+// Save node-spotify-api libray into a variable
+const Spotify = require('node-spotify-api');
+
+// Create Spotify object and save it into a variable
+const spotify = new Spotify(keys.spotify);
+
+const command = process.argv[2];
+
+// Decide which command will run
+switch (command) {
+    case "concert-this":
+        concertThis();
+        break;
+
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+    default:
+        console.log("\n------------------ERROR------------------");
+        console.log("Please use one of the following commands:");
+        console.log("    concert-this");
+        console.log("    spotify-this-song");
+        console.log("    movie-this");
+        console.log("    do-what-it-says");
+}
+
+function concertThis() {
+    console.log("In concertThis");
+}
+
+function spotifyThisSong() {
+    console.log("In spotifyThisSong");
+}
+
+function movieThis() {
+    console.log("In movieThis");
+}
+
+function doWhatItSays() {
+    console.log("In doWhatItSays");
+}
