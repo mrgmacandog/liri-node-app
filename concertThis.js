cd354d8e00ceeb100561ab72762a911f0c9af409
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-05-01 19:39:20 -0700

    Display all event details for an artist

--- concertThis.js
+++ concertThis.js
@@ -1,9 +1,13 @@
 // Include the axios npm package
 const axios = require("axios");
 
+
+const moment = require("moment");
+// moment().format();
+
 // Function 
 function concertThis() {
-    console.log("In concertThis.js")
+    // console.log("In concertThis.js")
 
     // Save the fourth value in the array as the artist
     let artist = process.argv[3];
@@ -18,7 +22,31 @@ function concertThis() {
     let queryUrl = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
 
     // TEMP LOGGING URL AND MANUALLY CHECKING IF LINK IS CORRECT
-    console.log(queryUrl);
+    // console.log(queryUrl);
+
+    // Create request with axios with URL to the API
+    axios.get(queryUrl)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data, null, 4));
+
+            let events = response.data;
+
+            for (let i = 0; i < events.length; i++) {
+                let event = events[i];
+
+                console.log();
+                console.log(event.venue.name);
+                console.log(`${event.venue.city}, ${event.venue.region} ${event.venue.country}`)
+                // console.log(event.datetime);
+                let momentEventTime = moment(event.datetime, "YYYY-MM-DDTHH:mm:ss");
+                let momentEventTimeFormatted = momentEventTime.format("MM/DD/YYYY h:mm a");
+                console.log(momentEventTimeFormatted);
+
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 };
 
 // Export the concertThis function so that it can be included in other files
